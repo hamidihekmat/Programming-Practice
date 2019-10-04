2119357634482ca3c73de28818b9a6b4fa34a6cf
Hekmat Hamidi <theonehamidi@gmail.com>
2019-10-04 14:37:08 -0400

    fixed game

--- Days/day18/rock.py
+++ Days/day18/rock.py
@@ -3,27 +3,33 @@
 import random
 
 
-choices = {'rock': 1, 'paper': 2, 'scissors': 3}
+def game(p1, p2):
+    running = True
+    while running:
+        print('robot : ', choices[p1])
+        print('you : ', choices[p2])
+        if p1 == p2:
+            print('Tie')
+            break
+        if p2 == 3 or p1 == 3:
+            if p1 == 1 or p2 == 1:
+                if not (p1 > p2):
+                    print('robot wins', choices[p1])
+                    break
+                if not (p1 < p2):
+                    print('you win', choices[p2])
+                    break
+        if p1 > p2:
+            print('robot wins yay', choices[p1])
+            break
+        if p2 > p1:
+            print('you win', choices[p2])
+            break
 
 
-running = True
-while running:
+if __name__ == '__main__':
+    choices = {1: 'rock', 2: 'paper', 3: 'scissors'}
     p1 = random.randint(1, 3)
-    p2 = int(input("Please choose {'rock': 1, 'paper': 2, 'scissors': 3}: "))
-    print('robot : ', p1)
-    print('you : ', p2)
-    if p1 == p2:
-        print('Tie')
-        break
-    if p2 == 3 or p1 == 3:
-        if p1 == 1:
-            if not (p1 > p2):
-                print('robot wins')
-                break
-            if not (p1 < p2):
-                print('you win')
-                break
-    if p1 > p2:
-        print('robot wins yay')
-    if p2 > p1:
-        print('you win')
+    p2 = int(
+        input("Please choose {'rock': 1, 'paper': 2, 'scissors': 3}: "))
+    game(p1, p2)
